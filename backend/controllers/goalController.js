1c729334db018c9a67c92ca0077916b2783375ae
ololadea0 <opeyemifawaz11@gmail.com>
2026-03-17 13:21:01 +0100

    Initial REST API for goals

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,60 @@
+import expressAsyncHandler from "express-async-handler";
+import Goal from "../models/goalModel.js";
+
+// @ desc Get Goals
+// @route GET /api/goals
+// @access Private
+const getGoals = expressAsyncHandler(async (req, res) => {
+    const goals = await Goal.find();
+    res.status(200).json(goals);
+});
+
+// @ desc sett Goals
+// @route POST /api/goals
+// @access Private
+const setGoal = expressAsyncHandler(async (req, res) => {
+    if (!req.body)
+    {
+        res.status(400);
+        throw new Error('Please add a text field');
+    }
+    const goal = await Goal.create({
+        text: req.body.text
+    });
+    res.status(200).json(goal);
+});
+
+// @ desc Update Goals
+// @route PUT /api/goals
+// @access Private
+const updateGoal = expressAsyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal)
+    {
+        res.status(404);
+        throw new Error('Goal not found');
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedGoal);
+});
+
+// @ desc Delete Goals
+// @route DELETE /api/goals
+// @access Private
+const deleteGoal = expressAsyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal)
+    {
+        res.status(404);
+        throw new Error('Goal not found');
+    }
+    await Goal.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+});
+
+
+export {
+    getGoals, setGoal, updateGoal, deleteGoal
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/goalRoutes.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', router);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
